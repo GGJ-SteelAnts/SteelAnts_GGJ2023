2cfdf29ddddea210546e3f053daaa83ce692c007
Imendin <silentislp@gmail.com>
2023-02-03 20:27:35 +0100

    complete basic menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,10 +7,23 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     public int startSceneIndex;
+    public GameObject menuPanel;
 
     public void StartGame()
     {
         SceneManager.LoadScene(startSceneIndex, LoadSceneMode.Single);
+    }
+
+    public void BackToMenu(GameObject actualWindow)
+    {
+        actualWindow.SetActive(false);
+        menuPanel.SetActive(true);
+    }
+
+    public void GoToOther(GameObject openWindow)
+    {
+        menuPanel.SetActive(false);
+        openWindow.SetActive(true);
     }
 
     public void EndGame()
